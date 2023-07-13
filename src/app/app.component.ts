61cb79a4c2b9e76bb146a0bd4592f4e2988e40fe
abhishekpyasi <pyasi.abhishek@gmail.com>
2023-07-13 17:17:35 +0530

    before routing section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,27 @@ import {
   Component,
   ViewChild,
   ViewContainerRef,
+  Inject,
+  OnInit,
 } from "@angular/core";
 import { RoomsComponent } from "./rooms/rooms.component";
+import { localStorageToken } from "./localstorage.token";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // ngAfterViewChecked(): void {
   //   this.vcr.createComponent(RoomsComponent);
   // }
   title = "compComm";
+
+  constructor(@Inject(localStorageToken) private localStorage: Storage) {}
+  ngOnInit(): void {
+    this.localStorage.setItem("name", "Hilton Hotel");
+  }
 
   @ViewChild("user", { read: ViewContainerRef, static: true })
   vcr!: ViewContainerRef;
